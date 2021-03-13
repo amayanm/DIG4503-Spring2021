6980615b089557c5a099cb216db0b1623cf3517a
amayanm <74217770+amayanm@users.noreply.github.com>
2021-03-13 01:52:33 -0500

    lab7

---
+++ lab7/index.js
@@ -0,0 +1,53 @@
+import Express from 'express';
+
+const App = Express();
+const port = 45030;
+
+const names = [
+    'Cortney',
+    'Dewayne',
+    'Trenton',
+    'Pamala',
+    'Ettie',
+    'Errol',
+    'Lorrie',
+    'Hang',
+    'Lauryn',
+    'Caterina',
+    'Isa',
+    'Marcela'
+];
+
+App.get("/people/:person", (req, res) => {
+    let result = {name: "not found"};
+
+    names.forEach((value) => {
+        if(value == req.params.person){
+            result = {name: value};
+        }
+    });
+
+    res.json(result);
+});
+
+App.get("/search/:name", (req, res) => {
+    let result = {search: "not found"};
+
+    let arrayResult = [];
+
+    names.forEach((value) => {
+        if(value.includes(req.params.name)){
+            arrayResult.push(value);
+        }
+    });
+
+    if(arrayResult.length > 0) {
+        result= {search: arrayResult}
+    }
+
+    res.json(result);
+});
+
+App.listen(port, () => {
+    console.log("Server is running!");
+});
