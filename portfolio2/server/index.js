5f0343bfa91007ed9af60f5299c4126072d21804
amayanm <74217770+amayanm@users.noreply.github.com>
2021-04-30 23:43:39 -0400

    portfolio2

--- portfolio2/server/index.js
+++ portfolio2/server/index.js
@@ -14,7 +14,7 @@ db.connect("portfolio2", "AmayaMorazan");
 datab.connect("sample_mflix", "movies");
 
 
-//Database Buddy - createOne -create a personal profile
+//Database Buddy - createOne -creating a movie
 App.put("/movie/:title", async (req, res) => {
     const title = req.params.title;
 
@@ -35,7 +35,7 @@ App.get("/movie/:title", async (req, res) => {
     res.json(result);
 });
 
-//Databse Budy - updateOne -updating profile
+//Databse Budy - updateOne -updating movie
 App.patch("/movie/:title", async (req, res) => {
     const title = req.params.title;
 
